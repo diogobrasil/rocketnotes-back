96cb6986dc86ca958d6f18e79e37aec2f17df301
diogo <diogobrasildasilva@gmail.com>
2024-05-29 05:52:34 -0300

    Adição do método de atualizar usuário.

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,5 +8,6 @@ const UsersController = require("../controllers/UsersController");
 const usersController = new UsersController();//Instaciação da classe UsersController.
 
 usersRouter.post("/", usersController.create);
+usersRouter.put("/:id", usersController.update);
 
 module.exports = usersRouter;//Exportação para que seja possível que outros modulos da api acessem as rotas de usuário.
